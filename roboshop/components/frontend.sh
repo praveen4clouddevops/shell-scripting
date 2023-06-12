4495149c8763182e6ddde5e1cb83cf35160f2117
telaprolup@gmail.com <AMMAnannatnp9#>
2023-06-12 07:35:35 +0530

    fonrtend

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,8 +1,8 @@
 #!/bib/bash
 
 ID=$(id -u)
-if [id -ne 0] ;then
-echo -e"\e[31m this script is exepcted to be run by a root user or with a sudo previlege \e[0m"
+if [ $ID -ne 0] ;then
+echo -e "\e[31m this script is exepcted to be run by a root user or with a sudo previlege \e[0m"
    exit 1
    fi
 echo "installing nginx"
