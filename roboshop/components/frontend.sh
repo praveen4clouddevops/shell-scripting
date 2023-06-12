5c982d045d1f1fc8ee87662fc996dfed89710411
telaprolup@gmail.com <AMMAnannatnp9#>
2023-06-12 07:32:27 +0530

    frontend.sh

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,2 +1,9 @@
 #!/bib/bash
-echo "i am frontend"
+
+ID=$(id -u)
+if [id -ne 0] ;then
+echo -e"\e[31m this script is exepcted to be run by a root user or with a sudo previlege \e[0m"
+   exit 1
+   fi
+echo "installing nginx"
+yum install nginx -y
