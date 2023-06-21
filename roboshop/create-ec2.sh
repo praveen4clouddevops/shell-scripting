03eb8a03b9fde6c85cbd5138e38fb26b6736f6d8
telaprolup@gmail.com <AMMAnannatnp9#>
2023-06-21 17:10:59 +0530

    auto push

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -4,4 +4,5 @@
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
 echo -e  "AMI ID used to launch the ec2 is \e[32m $AMI_ID \e[0m"
+echo -e "**** launching server****"
 aws ec2 run-instances  --image-id ${AMI_ID}  --instance-type t2.micro
