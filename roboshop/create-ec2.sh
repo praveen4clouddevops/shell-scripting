ef2d6c3aaa85621b452a2fa108e3636050544e3a
telaprolup@gmail.com <AMMAnannatnp9#>
2023-06-21 17:05:53 +0530

    auto push

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -2,5 +2,5 @@
 
 # AMI_ID="ami-0c1d144c8fdd8d690"
 
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId')
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
 echo "AMI_ID is $AMI_ID"
